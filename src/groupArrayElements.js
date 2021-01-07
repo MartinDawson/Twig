b31d9e17b0eb41db9f727d760dae3c0cf7051357
Martin Dawson <MDawson@marketaxess.com>
2021-01-07 17:39:56 +0000

    added docs for the public function

--- src/groupArrayElements.js
+++ src/groupArrayElements.js
@@ -6,6 +6,14 @@ const getChunksOfArray = (array, size) =>
     return acc;
   }, []);
 
+/**
+ *
+ * @param {An array of any type.} arrayElements
+ * @param {Should be a positive number. This is the number of slices you want to
+ * split the array into.} numberOfSlices
+ * @returns {An array of array chunks if you supply valid parameters. If invalid
+ * parameters are supplied it will return an empty array.}
+ */
 const groupArrayElements = (arrayElements, numberOfSlices) => {
   if (
     arrayElements.length === 0 ||
